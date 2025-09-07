a0ae3073804a421ff11e9ebeb5f411d0730384d1
Nsoedo Chioma Naomi <128978430+Cnsoedo@users.noreply.github.com>
2025-09-08 00:38:02 +0100

    improved Javascript code

--- SRC/script.js
+++ SRC/script.js
@@ -14,7 +14,7 @@ function generateHealth(event) {
   let apiKey = "4577128dt8o04b4f4924aca3faecb69f";
 
   let context =
-    "You are a health expert and love a healthy lifestyle.Your mission is to generate a 4 line health tips in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the health tips. Sign the health tips with 'Shecodes AI' inside a <strong> element at the end of the health tips and NOT at the beginning";
+    "You are a health expert and love a healthy lifestyle.Your mission is to generate a 4 line health tips in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the health tips. Sign the health tips with 'Naomi AI' inside a <strong> element at the end of the health tips and NOT at the beginning";
   let prompt = `User instructions are: Generate tips about  ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
