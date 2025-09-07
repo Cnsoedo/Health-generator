bc2986fff65c4673daa695a571b81ae8ca74f06b
Nsoedo Chioma Naomi <128978430+Cnsoedo@users.noreply.github.com>
2025-09-07 21:51:14 +0100

    Added the AI API integration

--- SRC/script.js
+++ SRC/script.js
@@ -1,11 +1,29 @@
-function generateHealth(event) {
-  event.preventDefault();
+function displayHealth(response) {
+  console.log("health generated");
   new Typewriter("#health", {
-    strings: "Drink lots of water to stay dehydrated",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
-    curosr: "",
+    cursor: "",
   });
+}
+
+function generateHealth(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "4577128dt8o04b4f4924aca3faecb69f";
+
+  let context =
+    "You are a health expert and love a healthy lifestyle.Your mission is to generate a 4 line health tips in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the health tips. Sign the health tips with 'Shecodes AI' inside a <strong> element at the end of the health tips and NOT at the beginning";
+  let prompt = `User instructions are: Generate tips about  ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating health tips");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiUrl).then(displayHealth);
 }
 
 let healthFormElement = document.querySelector("#health-generator-form");
